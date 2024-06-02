baf4c29f2da4b69e13e8afcee427252c43e2ba7c
webfront <webfront65@gmail.com>
2024-06-02 16:31:42 +0700

    update commit

--- js/app.js
+++ js/app.js
@@ -5,4 +5,60 @@ function OpenNav(){
         document.getElementById("nav").style.width = "0%";
 }
 
+var swiper = new Swiper(".mySwiper-top", {
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+        
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        768: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        },
+    },
+});
+        
+var swiper = new Swiper(".mySwiper-bottom", {
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+        
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        },
+    },
+});
+
 
